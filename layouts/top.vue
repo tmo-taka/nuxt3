226d21a71a6315548990a2c1bbda1d882ae5dfc2
takahara tomoya <t.tkorock221@gmail.com>
2022-08-12 08:44:59 +0900

    漏れた

---
+++ layouts/top.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- NOTE: app.vueで使う際は<NuxtLayout>が必要 -->
+    <NuxtLayout>
+      <header class="font-bold py-6 px-3 text-left">ヘッダーです</header>
+      <NuxtPage />
+      <footer class="font-bold py-6 px-3 text-right text-lg">フッターです</footer>
+    </NuxtLayout>
+  </div>
+</template>
